b5fa8d3f0833914aac34cc5d6e0c9c9627108636
Alexander Kuryavsky <santiagosmit228@gmail.com>
2023-08-10 19:17:09 +0200

    fix: added logs

--- src/stores/stores.service.ts
+++ src/stores/stores.service.ts
@@ -186,6 +186,7 @@ export class StoresService {
   async assignBot(id: string, assignBotDto: AssignBotDto) {
     console.log(33333333, id);
     console.log(44444444, assignBotDto);
+    console.log(55555555);
     return this.storeModel.findByIdAndUpdate(id, {
       ...assignBotDto
     }, { new: true }).exec()
